62283e2215a3ba6ddf2f4c1979f9dc14555272fd
Habib1998 <Habib.kasbati@gmail.com>
2020-06-21 17:52:27 +0500

    Expense-Tracker-App

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
+
+import { GlobalProvider } from "./context/GlobalState";
+
+
+
+const App = () => {
+  return (
+    <GlobalProvider>
+      <div className="box">
+      <Header />
+      <div className="container">
+      <Balance />
+      <IncomeExpenses />
+      <TransactionList />
+      <AddTransaction />
+    </div>
+    </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
